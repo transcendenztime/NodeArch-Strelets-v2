a478a771224d52391620f9ace408b4fbafef9b86
transcendenztime <transcendenztime@gmail.com>
2022-04-30 21:22:26 +0300

    [3095][система голосования] stat service

--- 3095/3095-server.js
+++ 3095/3095-server.js
@@ -9,17 +9,62 @@ const webserver = express();
 webserver.use(express.static(path.join(__dirname, "public")));
 // webserver.use(bodyParser.json());
 // webserver.use(bodyParser.urlencoded({extended:true}));
-webserver.use(express.urlencoded({extended:true}));
+webserver.use(express.urlencoded({extended: true}));
 
 const port = 3095;
 const jsonFilesPath = "jsonFiles";
 
-webserver.get("/variants",(req, res) => {
+const variantsFilePath = path.join(jsonFilesPath, "variants.json");
+const statFilePath = path.join(jsonFilesPath, "_stats.json");
+
+createInitialStatFile = () => {
+  const fileContent = [
+    {
+      id: 1,
+      value: "Английский",
+      count: 0,
+    },
+    {
+      id: 2,
+      value: "Немецкий",
+      count: 0,
+    },
+    {
+      id: 3,
+      value: "JavaScript!!!",
+      count: 0,
+    },
+    {
+      id: 1,
+      value: "Никакой(",
+      count: 0,
+    },
+  ];
+
+  fs.writeFileSync(statFilePath, JSON.stringify(fileContent));
+};
+
+webserver.get("/variants", (req, res) => {
   // const variants = fs.readFileSync(path.join(jsonFilesPath, "variants.json"), "utf8");
   // res.setHeader("Content-Type", "application/json");
-  const variants = fs.readFileSync(path.join(jsonFilesPath, "variants.json"));
+  const variants = fs.readFileSync(variantsFilePath);
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.send(variants);
+});
+
+webserver.post("/stat", (req, res) => {
+  let stat;
+  // если файл статистики существует
+  if (fs.existsSync(statFilePath)) {
+    stat = fs.readFileSync(statFilePath);
+  } else {
+    // если файл статистики не существует, создадим его базовый вариант
+    createInitialStatFile();
+    stat = fs.readFileSync(statFilePath);
+  }
+
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.send(stat);
 });
 
 webserver.listen(port, () => {
--- 3095/public/3095-front.js
+++ 3095/public/3095-front.js
@@ -1,13 +1,19 @@
 window.onload = () => {
-  getVariants();
+  variantsService();
+  statService();
 };
 
-getVariants = () => {
+variantsService = () => {
   fetch("/variants", {method: "GET"})
     .then(response => response.json())
     .then(data => {
-      const variants = data.map( variant =>
-        `<input type="button" id="${variant.id}_" value="${variant.value}"><br />`
-      ).join(`<br />`);
-      document.getElementById("variants").innerHTML = variants});
+      const variants = data
+        .map(variant => `<input type="button" id="${variant.id}_" value="${variant.value}"><br />`)
+        .join(`<br />`);
+      document.getElementById("variants").innerHTML = variants;
+    });
+};
+
+statService = () => {
+  fetch("/stat", {method: "POST"});
 };
