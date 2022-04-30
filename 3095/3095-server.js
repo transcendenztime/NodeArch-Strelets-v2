ea4743cb3d3014eb59809b0af255eae29f9e7f78
transcendenztime <transcendenztime@gmail.com>
2022-04-30 22:39:10 +0300

    [3095][система голосования] vote service

--- 3095/3095-server.js
+++ 3095/3095-server.js
@@ -1,47 +1,48 @@
 const express = require("express");
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
 const webserver = express();
 
+// отдаем index.html как статику
 webserver.use(express.static(path.join(__dirname, "public")));
-// webserver.use(bodyParser.json());
+webserver.use(bodyParser.json());
 // webserver.use(bodyParser.urlencoded({extended:true}));
-webserver.use(express.urlencoded({extended: true}));
+// webserver.use(express.urlencoded({extended: true}));
 
 const port = 3095;
 const jsonFilesPath = "jsonFiles";
-
 const variantsFilePath = path.join(jsonFilesPath, "variants.json");
-const statFilePath = path.join(jsonFilesPath, "_stats.json");
+const statsFilePath = path.join(jsonFilesPath, "_stats.json");
 
+// функция нужна для того, чтобы на сервере создался файл с нулевой статисткой
 createInitialStatFile = () => {
   const fileContent = [
     {
-      id: 1,
+      id: "1",
       value: "Английский",
       count: 0,
     },
     {
-      id: 2,
+      id: "2",
       value: "Немецкий",
       count: 0,
     },
     {
-      id: 3,
+      id: "3",
       value: "JavaScript!!!",
       count: 0,
     },
     {
-      id: 1,
+      id: "4",
       value: "Никакой(",
       count: 0,
     },
   ];
 
-  fs.writeFileSync(statFilePath, JSON.stringify(fileContent));
+  fs.writeFileSync(statsFilePath, JSON.stringify(fileContent));
 };
 
 webserver.get("/variants", (req, res) => {
@@ -55,16 +56,43 @@ webserver.get("/variants", (req, res) => {
 webserver.post("/stat", (req, res) => {
   let stat;
   // если файл статистики существует
-  if (fs.existsSync(statFilePath)) {
-    stat = fs.readFileSync(statFilePath);
+  if (fs.existsSync(statsFilePath)) {
+    stat = fs.readFileSync(statsFilePath);
   } else {
-    // если файл статистики не существует, создадим его базовый вариант
+    // если файл статистики не существует, создадим его базовый вариант (где у всех вариантов по 0 голосов)
     createInitialStatFile();
-    stat = fs.readFileSync(statFilePath);
+    stat = fs.readFileSync(statsFilePath);
   }
 
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.send(stat);
+});
+
+webserver.post("/vote", (req, res) => {
+  let fileContent = fs.readFileSync(statsFilePath, "utf8");
+  fileContent = JSON.parse(fileContent);
+
+  /* fileContent.forEach((stat) => {
+    console.log(stat)
+    if(stat.id === req.body.id) {
+      stat.count = stat.count + 1;
+    }
+  });
+
+  fs.writeFileSync(statsFilePath, JSON.stringify(fileContent));
+  res.status(200).end();*/
+
+  const elem = fileContent.find((stat) => {
+    return stat.id === req.body.id;
+  });
+
+  if(elem) {
+    elem.count = elem.count + 1;
+    fs.writeFileSync(statsFilePath, JSON.stringify(fileContent));
+    res.status(200).end();
+  }
+
+  res.status(530).end();
 });
 
 webserver.listen(port, () => {
--- 3095/public/3095-front.js
+++ 3095/public/3095-front.js
@@ -8,12 +8,37 @@ variantsService = () => {
     .then(response => response.json())
     .then(data => {
       const variants = data
-        .map(variant => `<input type="button" id="${variant.id}_" value="${variant.value}"><br />`)
+        .map(
+          variant =>
+            `<input type="button" id="${variant.id}_" value="${variant.value}" onclick=voteService("${variant.id}")><br />`,
+        )
         .join(`<br />`);
       document.getElementById("variants").innerHTML = variants;
     });
 };
 
+updateStatistics = data => {
+  let stats = data
+    .map(stat => `<div><span>${stat.value}: </span><span style="font-weight: bold;">${stat.count}</span></div>`)
+    .join(`<br>`);
+  document.getElementById("statistics").innerHTML = stats;
+};
+
 statService = () => {
-  fetch("/stat", {method: "POST"});
+  fetch("/stat", {method: "POST"})
+    .then(response => response.json())
+    .then(data => {
+      updateStatistics(data);
+    });
+};
+
+voteService = id => {
+  const body = JSON.stringify({id: id});
+  fetch("/vote", {method: "POST", headers: {"Content-Type": "application/json"}, body: body}).then(response => {
+    if (response.ok) {
+      statService();
+    } else {
+      alert(`Произола ошибка ${response.status}, нет такого варианта для голосования!`);
+    }
+  });
 };
