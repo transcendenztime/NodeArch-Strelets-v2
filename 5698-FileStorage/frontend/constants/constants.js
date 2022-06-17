9c26a173be63c58af4aa873e24859abbcb99ae21
transcendenztime <transcendenztime@gmail.com>
2022-06-17 22:22:27 +0300

    [5698][FileStorage] 5698 delete

--- 5698-FileStorage/frontend/constants/constants.js
+++
@@ -1,6 +0,0 @@
-export const Methods = {
-  GET: "GET",
-  POST: "POST",
-};
-
-export const wsUrl = process.env.NODE_ENV === "development" ? "ws://localhost:5699" : "ws://128.199.37.165:5699";
--- 5698-FileStorage/frontend/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from "./Main/Main";
-
-ReactDOM.render(<Main/>, document.getElementById('container'));
